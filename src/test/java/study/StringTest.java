37e4593cd5b81ce564366635e3a1d90d2ff02e4a
seongjinOliver <yoosngj@gmail.com>
2024-07-04 00:46:38 +0900

    split test

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,5 +1,6 @@
 package study;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -9,5 +10,12 @@ public class StringTest {
     void replace() {
         String actual = "abc".replace("b", "d");
         assertThat(actual).isEqualTo("adc");
+    }
+
+    @Test
+    void split() {
+        String[] split = "1,2".split(",");
+        assertThat(split).contains("2", "1");
+        assertThat(split).containsExactly("1", "2");
     }
 }
