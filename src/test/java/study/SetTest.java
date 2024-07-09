dd04678fe66eac9e22d075d1e956f9e87b599d19
seongjinOliver <yoosngj@gmail.com>
2024-07-10 00:46:11 +0900

    Set test

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -1,12 +1,45 @@
 package study;
 
-import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
-public class SetCollectionTest {
-    @DisplayName("")
-    @Test
-    void test() {
-        
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SetTest {
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
     }
+
+    @Test
+    void setSize() {
+        assertThat(numbers.size()).isEqualTo(3);
+    }
+
+    @Test
+    void setContains() {
+        assertThat(numbers.contains(1)).isTrue();
+        assertThat(numbers.contains(2)).isTrue();
+        assertThat(numbers.contains(3)).isTrue();
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"test:test", "tEst:test", "Java:java"}, delimiter = ':')
+    void toLowerCase_ShouldGenerateTheExpectedLowercaseValue(String input, String expected) {
+        String actualValue = input.toLowerCase();
+        assertThat(actualValue).isEqualTo(expected);
+    }
+
 }
