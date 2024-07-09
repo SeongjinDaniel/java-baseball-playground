47fc033eb2abda764d63a7ce0039a8834f701083
seongjinOliver <yoosngj@gmail.com>
2024-07-10 00:45:59 +0900

    String test

---
+++ src/test/java/study/SetTest.java
@@ -0,0 +1,12 @@
+package study;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class SetCollectionTest {
+    @DisplayName("")
+    @Test
+    void test() {
+        
+    }
+}
--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -3,7 +3,7 @@ package study;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.*;
 
 public class StringTest {
     @Test
@@ -16,6 +16,37 @@ public class StringTest {
     void split() {
         String[] split = "1,2".split(",");
         assertThat(split).contains("2", "1");
+    }
+
+    @Test
+    void split2() {
+        String[] split = "1,2".split(",");
         assertThat(split).containsExactly("1", "2");
+    }
+
+    @DisplayName("charAt 메서드에서 assertThatThrownBy를 사용하여 Exception을 처리한다.")
+    @Test
+    void charAtException() {
+        String charAt = "abc";
+        int index = 12;
+
+        assertThatThrownBy(() -> {
+            char c = charAt.charAt(index);
+            assertThat(c).isEqualTo('c');
+        }).isInstanceOf(StringIndexOutOfBoundsException.class)
+            .hasMessageContaining("%d", index);
+    }
+
+    @DisplayName("charAt 메서드에서 assertThatExceptionOfType 사용하여 Exception을 처리한다.")
+    @Test
+    void charAtException2() {
+        String charAt = "abc";
+        int index = 12;
+
+        assertThatExceptionOfType(StringIndexOutOfBoundsException.class)
+            .isThrownBy(() -> {
+                char c = charAt.charAt(index);
+                assertThat(c).isEqualTo('c');
+            }).withMessageMatching("String index out of range: \\d+");
     }
 }
